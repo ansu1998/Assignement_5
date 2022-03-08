a9b4628145bbe6069d180a7ee62c70fd34d37547
ansu1998 <ansuapillai1998@gmail.com>
2022-03-08 10:23:10 +0530

    First Commit

---
+++ src/com/company/Program_1.java
@@ -0,0 +1,20 @@
+package com.company;
+
+import java.util.HashSet;
+import java.util.*;
+
+public class Program_1 {
+
+    public static void main(String[] args)
+    {
+        Set<String> hash_Set = new HashSet<String>();
+
+        hash_Set.add("Geeks");
+        hash_Set.add("For");
+        hash_Set.add("Geeks");
+        hash_Set.add("Example");
+        hash_Set.add("Set");
+
+        System.out.println(hash_Set);
+    }
+}
